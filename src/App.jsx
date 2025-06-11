236bffd68b2225e85636aa5538fafc493b15338f
Eddan <edvin.runhellen1@gmail.com>
2025-06-11 19:28:49 +0200

    added mui component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
-
+import { Stack, Button } from '@mui/material';
 function App() {
 	return (
 		<div className="App">
 			<h1>Techover Self Made - Spotify</h1>
+			<Stack spacing={2} direction="row">
+				<Button variant="text">Text</Button>
+				<Button variant="contained">Contained</Button>
+				<Button variant="outlined">Outlined</Button>
+			</Stack>
 		</div>
 	);
 }
